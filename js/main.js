58cb5ad70361e14378c86d8169e8f222c6be5548
Andrey Kolomoets <kolomoets.and@gmail.com>
2020-03-21 22:16:09 +0200

    mobile

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var x = window.matchMedia("(max-width:900px)");
+    var x = window.matchMedia("(max-width:991px)");
     if (x.matches) {
         $('.dropdown_link li').show();
     } else {
@@ -19,4 +19,62 @@ $(document).ready(function() {
     } else {
         $('.navbar').css("background-image", "url('images/menu-bg.png')");
     };
+});
+
+$(document).ready(function() {
+    var checkBox = $("#customCheck1");
+    var sendBtn = $(".submit");
+    
+    $("#customCheck1").click(function() {
+		if (checkBox.is(":checked")) {
+			sendBtn.removeAttr("disabled");
+		} else {
+			sendBtn.attr("disabled", "disabled");
+		};
+    });
+    
+    var checkBox1 = $("#customCheck2");
+    var sendBtn1 = $("#submit2");
+    
+    $("#customCheck2").click(function() {
+		if (checkBox1.is(":checked")) {
+			sendBtn1.removeAttr("disabled");
+		} else {
+			sendBtn1.attr("disabled", "disabled");
+		};
+    });
+    
+    var checkBox2 = $("#customCheck3");
+    var sendBtn2 = $("#submit3");
+    
+    $("#customCheck3").click(function() {
+		if (checkBox2.is(":checked")) {
+			sendBtn2.removeAttr("disabled");
+		} else {
+			sendBtn2.attr("disabled", "disabled");
+		};
+    });
+    
+    var checkBox3 = $("#customCheck4");
+    var sendBtn3 = $("#submit4");
+    
+    $("#customCheck4").click(function() {
+		if (checkBox3.is(":checked")) {
+			sendBtn3.removeAttr("disabled");
+		} else {
+			sendBtn3.attr("disabled", "disabled");
+		};
+	});
+});
+
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "100%";
+};
+
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+};
+
+$('.dropdown_item').click( function(){
+    document.getElementById("mySidepanel").style.width = "0";
 });
